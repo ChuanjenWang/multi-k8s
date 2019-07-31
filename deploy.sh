4100922e87962a48fc96394f906c955504019af3
Spencer.Wang <chuanjen.wang@gmail.com>
2019-07-31 12:14:39 +0800

    added deployment scripts

---
+++ deploy.sh
@@ -0,0 +1,15 @@
+docker build -t spencerwang/multi-client:latest -t spencerwang/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t spencerwang/multi-server:latest -t spencerwang/multi-server:$SHA -f ./client/Dockerfile ./server
+docker build -t spencerwang/multi-worker:latest -t spencerwang/multi-workwe:$SHA -f ./worker/Dockerfile ./worker
+docker push spencerwang/multi-client:latest
+docker push spencerwang/multi-server:latest
+docker push spencerwang/multi-worker:latest
+
+docker push spencerwang/multi-client:$SHA
+docker push spencerwang/multi-server:$SHA
+docker push spencerwang/multi-worker:$SHA
+
+kubectl apply -f k8s
+kubectl set image deployment/server-deployment server=spencerwang/multi-server:$SHA
+kubectl set image deployment/client-deployment client=spencerwang/multi-client:$SHA
+kubectl set image deployment/worker-deployment worker=spencerwang/multi-worker:$SHA
